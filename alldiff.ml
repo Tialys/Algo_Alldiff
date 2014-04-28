3923138dbb63175622fa9cd3743674c7aeb173b7
Tialys <clement.vincent1@gmail.com>
2014-04-28 15:42:12 +0200

    ajout de la fonction matching_max

--- alldiff.ml
+++ alldiff.ml
@@ -28,14 +28,31 @@ let neighbors (Graph(x,y,e)) n =
 
 let rec remove_edge n es = match es with
   | [] -> []
-  | (x,y)::es -> if n == x then remove_edge y es else 
-    if n == y then es else remove_edge n es
+  | (x,y)::es -> if n = x then remove_edge y es else 
+    if n = y then es else remove_edge n es
 ;;
 
 let rec x_matching y es = match es with
         | [] -> y
         | (x,s)::e -> if (s = y) then x else x_matching y e
 ;;
+
+let remove e l = 
+  let rec remove' e l aux = match l with
+    | [] -> aux
+    | el::t -> if e = el then (aux @ t) else remove' e t (el::aux)
+  in
+  remove' e l []
+;;
+
+let rec upgrade_matching matchingEdges path = match path with
+  | [] -> matchingEdges
+  | el::t -> if (mem el matchingEdges) 
+      then upgrade_matching (remove el matchingEdges) t 
+      else upgrade_matching (el::matchingEdges) t
+;;
+
+
 (* find_first_not : node list -> node list -> node 
  * find_first_not l1 l2 renvoie le premier element de l2 qui n'est pas dans l1
  *)
@@ -62,8 +79,7 @@ let matching (Graph(x,y,e)) =
 
 let alternating_path x g matchingEdes matchingNodes =
   let rec alternating_path' x g matchingEdges matchingNodes visited path =
-    let nx = (filter (fun el -> (not (mem el visited))) (neighbors g x)) in
-    match nx with
+    match (filter (fun el -> (not (mem el visited))) (neighbors g x)) with
     | [] -> (let new_path = remove_edge x path in match new_path with
         | (new_x,_)::q -> alternating_path' new_x g matchingEdges matchingNodes visited new_path
         | _ -> [])
@@ -76,7 +92,17 @@ let alternating_path x g matchingEdes matchingNodes =
 ;;
 
 
-
+let matching_max g matchingEdges =
+  let rec matching_max' (Graph(x,_,_) as g) matchingEdges =
+    let (matchingNodesx, matchingNodesy) = split matchingEdges in
+      match (filter (fun el -> (not (mem el matchingNodesx))) x) with
+      | [] -> matchingEdges
+      | el::xs -> let path = alternating_path el g matchingEdges (matchingNodesx @ matchingNodesy) in match path with
+        | [] -> []
+        | _ -> matching_max' g (upgrade_matching matchingEdges path)
+  in
+  matching_max' g matchingEdges
+;;
 
 
 
--- test.ml
+++ test.ml
@@ -15,19 +15,24 @@ let rec printe_list = function
 ;;
 
 
-let graphe1 = Graph([Node(1);Node(2);Node(3);Node(4);Node(5);Node(6)],[Node(7);Node(8);Node(9);Node(10);Node(11);Node(12);Node(13)],[Edge((Node(1),Node(7)));Edge((Node(1),Node(8)));Edge((Node(2),Node(8)));Edge((Node(2),Node(9)));Edge((Node(3),Node(7)));Edge((Node(3),Node(9)));Edge((Node(4),Node(8)));Edge((Node(4),Node(10)));Edge((Node(5),Node(9)));Edge((Node(5),Node(10)));Edge((Node(5),Node(11)));Edge((Node(5),Node(12)));Edge((Node(6),Node(12)));Edge((Node(6),Node(13)))])
+let graphe1 = Graph([Node(1);Node(2);Node(3);Node(4);Node(5);Node(6)],[Node(7);Node(8);Node(9);Node(10);Node(11);Node(12);Node(13)],[Edge((Node(1),Node(7)));Edge((Node(1),Node(8)));(*Edge((Node(2),Node(8)));*)Edge((Node(2),Node(9)));(*Edge((Node(3),Node(7)));*)Edge((Node(3),Node(9)));Edge((Node(4),Node(8)));Edge((Node(4),Node(10)));Edge((Node(5),Node(9)));Edge((Node(5),Node(10)));Edge((Node(5),Node(11)));Edge((Node(5),Node(12)));Edge((Node(6),Node(12)));Edge((Node(6),Node(13)))])
 
 
 let graphe2 = Graph([Node(1);Node(2);Node(3);Node(4)],[Node(5);Node(6);Node(7);Node(8)], [Edge((Node(1),Node(5)));Edge((Node(1),Node(6)));Edge((Node(2),Node(5)));Edge((Node(2),Node(6)));Edge((Node(3),Node(6)));Edge((Node(3),Node(7)));Edge((Node(4),Node(7)));Edge((Node(4),Node(8)))])
 ;;
 
-let graphe3 = Graph([Node(1);Node(2);Node(3)],[Node(4);Node(5);Node(6)],[Edge((Node(1),Node(4)));Edge((Node(1),Node(5)));Edge((Node(1),Node(6)));Edge((Node(2),Node(4)));Edge((Node(2),Node(5)));Edge((Node(3),Node(6)))])
+let (Graph(x3,y3,e3) as graphe3) = Graph([Node(1);Node(2);Node(3)],[Node(4);Node(5);Node(6)],[Edge((Node(1),Node(4)));Edge((Node(1),Node(5)));Edge((Node(1),Node(6)));Edge((Node(2),Node(4)));Edge((Node(2),Node(5)));Edge((Node(3),Node(6)))])
 ;;
 
 
 let (mnode1, medge1) = matching graphe1;;
 let (mnode2, medge2) = matching graphe2;;
 let (mnode3, medge3) = matching graphe3;;
+let (mnodex3,mnodey3) = split medge3;;
+
+let alp3 = alternating_path (Node(3)) graphe3 medge3 mnode3;;
+let upmatch3 = upgrade_matching medge3 alp3;;
+
 
 printn_list mnode1;print_string "\n";;
 printe_list medge1;print_string "\n";;
@@ -35,5 +40,9 @@ printn_list mnode2;print_string "\n";;
 printe_list medge2;print_string "\n";;
 printn_list mnode3;print_string "\n";;
 printe_list medge3;print_string "\n";;
-printe_list (alternating_path (Node(3)) graphe3 medge3 mnode3);;
-                       
+printe_list alp3;;
+printe_list (matching_max graphe3 []);;
+printe_list upmatch3;;
+printe_list (matching_max graphe3 upmatch3);; 
+printe_list (matching_max graphe2 []);;
+printe_list (matching_max graphe1 []);;                     
